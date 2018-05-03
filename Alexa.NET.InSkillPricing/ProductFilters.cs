2e28d7e0212a9dafbe6108202f31553a45d5ed44
Steven Pears <steven.pears@live.co.uk>
2018-05-03 21:51:52 +0100

    Add product filters

---
+++ Alexa.NET.InSkillPricing/ProductFilters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alexa.NET.InSkillPricing
+{
+    public class ProductFilters
+    {
+        public int? MaxResults { get; set; }
+        public string Entitled { get; set; }
+        public string Purchasable { get; set; }
+        public string ProductType { get; set; }
+
+        public bool AnySet => MaxResults.HasValue ||
+                              !string.IsNullOrWhiteSpace(Entitled) ||
+                              !string.IsNullOrWhiteSpace(Purchasable) ||
+                              !string.IsNullOrWhiteSpace(ProductType);
+    }
+}
